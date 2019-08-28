8b9e620563e912717ae51a6c0316f1287ff6d7f3
Chance Smith <chancesmith97@hotmail.com>
2019-08-28 23:56:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters 
+  belongs_to :network
+  
+  def actors_list
+    self.chracters.each do|x, y|
+      "#{x.first_name} #{y.last_name}"
+    end
+  end
   
 end
